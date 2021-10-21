838119d5a8b5691699833218806a5cc01f456e80
Shubham Singh Chouhan <64628073+shubhamsc11@users.noreply.github.com>
2021-10-21 14:08:14 +0530

    Update AsciiAlphabets.c

--- AsciiAlphabets.c
+++ AsciiAlphabets.c
@@ -1,40 +1,36 @@
-
 // C program to print alphabets
 
 #include <stdio.h>
   
-// Function to print the alphabet
-// in lower case
 void lowercaseAlphabets()
 {
     // for lowercase
     for (int c = 97; c <= 122; ++c)
+    {
         printf("%c ", c);
+    }
     printf("\n");
 }
-// Function to print the alphabet
-// in upper case
+
+// for upper case
 void uppercaseAlphabets()
 {
-  
-    // Run a loop from 65 to 90
     for (int c = 65; c <= 90; ++c)
-  
+    {
         // print its ascii values
         printf("%c ", c);
-  
+    }
     printf("\n");
 }
   
 // Driver program
 int main()
 {
-  
     printf("Uppercase Alphabets\n");
     uppercaseAlphabets();
   
     printf("Lowercase Alphabets\n");
-    lowercaseAlphabets();
+    lowercaseAlphabets(); 
   
     return 0;
 }
